2a69773ca3c553a7e2f5b255bdb067221db5d4a5
fzfking <48185056+fzfking@users.noreply.github.com>
2023-05-05 01:28:00 +0300

    feat: text write animation

---
+++ Extensions/UnityExtensions.cs
@@ -0,0 +1,30 @@
+﻿#if UNITY_2022_1_OR_NEWER
+using DG.Tweening;
+using TMPro;
+
+namespace FZFUI.UI.Extensions
+{
+    public static class TextMeshProExtensions
+    {
+        public static Tween AnimateTextWriting(this TextMeshProUGUI textComponent, string text, float duration)
+        {
+            textComponent.maxVisibleCharacters = 0;
+            textComponent.text = text;
+            return DOTween.To(() => textComponent.maxVisibleCharacters, 
+                x => textComponent.maxVisibleCharacters = x, 
+                text.Length, 
+                duration);
+        }
+        public static Tween AnimateTextWritingPerCharacterDuration(this TextMeshProUGUI textComponent, string text, float duration)
+        {
+            textComponent.maxVisibleCharacters = 0;
+            textComponent.text = text;
+            var fullDuration = duration * text.Length;
+            return DOTween.To(() => textComponent.maxVisibleCharacters, 
+                x => textComponent.maxVisibleCharacters = x, 
+                text.Length, 
+                fullDuration);
+        }
+    }
+}
+#endif
