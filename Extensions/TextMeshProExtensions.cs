39b386600abbc63074fec2e5ded73e7a80ade044
fzfking <48185056+fzfking@users.noreply.github.com>
2023-07-09 16:34:19 +0300

    feat: timespan extension

--- Extensions/TextMeshProExtensions.cs
+++ Extensions/TextMeshProExtensions.cs
@@ -1,4 +1,6 @@
 ﻿#if UNITY_2022_1_OR_NEWER
+using System;
+using System.Collections.Generic;
 using DG.Tweening;
 using TMPro;
 
@@ -24,6 +26,28 @@ namespace FZFUI.UI.Extensions
                 x => textComponent.maxVisibleCharacters = x, 
                 text.Length, 
                 fullDuration);
+        }
+        
+        public static string ToShortTimeSpan(this TimeSpan timeSpan)
+        {
+            var parts = new List<string>();
+
+            if (timeSpan.Days > 0)
+                parts.Add($"{timeSpan.Days} {(timeSpan.Days == 1 ? "day" : "days")}");
+
+            if (timeSpan.Hours > 0)
+                parts.Add($"{timeSpan.Hours} {(timeSpan.Hours == 1 ? "hour" : "hours")}");
+
+            if (timeSpan.Minutes > 0)
+                parts.Add($"{timeSpan.Minutes} {(timeSpan.Minutes == 1 ? "min" : "mins")}");
+
+            if (timeSpan.Seconds > 0)
+                parts.Add($"{timeSpan.Seconds} {(timeSpan.Seconds == 1 ? "sec" : "secs")}");
+            
+            if (timeSpan is { Milliseconds: > 0, Seconds: < 1 })
+                parts.Add($"{timeSpan.Milliseconds} {(timeSpan.Milliseconds == 1 ? "ms" : "ms")}");
+
+            return string.Join(" ", parts);
         }
     }
 }
