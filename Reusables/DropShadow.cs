3fc764aa630f4a7dcc7e48431af5b8b62a8577f1
fzfking <48185056+fzfking@users.noreply.github.com>
2023-07-22 14:52:56 +0300

    feat: dropshadow effect

---
+++ Reusables/DropShadow.cs
@@ -0,0 +1,132 @@
+﻿//Source: https://www.reddit.com/r/Unity3D/comments/dmxxai/ui_drop_shadow_script/
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FZFUI.Reusables
+{
+    [AddComponentMenu("UI/Effects/DropShadow", 14)]
+    public class DropShadow : BaseMeshEffect
+    {
+        [SerializeField]
+        private Color shadowColor = new(0f, 0f, 0f, 0.5f);
+ 
+        [SerializeField]
+        private Vector2 shadowDistance = new(1f, -1f);
+ 
+        [SerializeField]
+        private bool useGraphicAlpha = true;
+        public int iterations = 5;
+        public Vector2 shadowSpread = Vector2.one;
+
+#if UNITY_EDITOR
+        protected override void OnValidate()
+        {
+            EffectDistance = shadowDistance;
+            base.OnValidate();
+        }
+ 
+#endif
+ 
+        public Color EffectColor
+        {
+            get => shadowColor;
+            set
+            {
+                shadowColor = value;
+                if (graphic != null)
+                    graphic.SetVerticesDirty();
+            }
+        }
+ 
+        public Vector2 ShadowSpread
+        {
+            get => shadowSpread;
+            set
+            {
+                shadowSpread = value;
+                if (graphic != null)
+                    graphic.SetVerticesDirty();
+            }
+        }
+ 
+        public int Iterations
+        {
+            get => iterations;
+            set
+            {
+                iterations = value;
+                if (graphic != null)
+                    graphic.SetVerticesDirty();
+            }
+        }
+ 
+        public Vector2 EffectDistance
+        {
+            get => shadowDistance;
+            set
+            {
+                shadowDistance = value;
+ 
+                if (graphic != null)
+                    graphic.SetVerticesDirty();
+            }
+        }
+ 
+        public bool UseGraphicAlpha
+        {
+            get => useGraphicAlpha;
+            set
+            {
+                useGraphicAlpha = value;
+                if (graphic != null)
+                    graphic.SetVerticesDirty();
+            }
+        }
+
+
+        private void DropShadowEffect(List<UIVertex> verts)
+        {
+            var count = verts.Count;
+ 
+            var vertsCopy = new List<UIVertex>(verts);
+            verts.Clear();
+ 
+            for(var i=0; i<iterations; i++)
+            {
+                for(var v=0; v<count; v++)
+                {
+                    var vt = vertsCopy[v];
+                    var position = vt.position;
+                    var fac = i/(float)iterations;
+                    position.x *= (1 + shadowSpread.x*fac*0.01f);
+                    position.y *= (1 + shadowSpread.y*fac*0.01f);
+                    position.x += shadowDistance.x * fac;
+                    position.y += shadowDistance.y * fac;
+                    vt.position = position;
+                    Color32 color = shadowColor;
+                    color.a = (byte)(color.a /(float)iterations);
+                    vt.color = color;
+                    verts.Add(vt);
+                }
+            }
+
+            verts.AddRange(vertsCopy);
+        }
+ 
+        public override void ModifyMesh(VertexHelper vh)
+        {
+            if (!IsActive())
+                return;
+        
+            var output = new List<UIVertex>();
+            vh.GetUIVertexStream(output);
+ 
+            DropShadowEffect(output);
+ 
+            vh.Clear();
+            vh.AddUIVertexTriangleStream(output);
+        }
+    }
+}
